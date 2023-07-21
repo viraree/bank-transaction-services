71f025ce4561918e204cfd141080367f7f41f928
viraree <tao.wang@deckers.com>
2023-07-21 21:08:04 +0800

    sql

--- db-script.sql
+++ db-script.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE bank_nosql;
+
+USE bank_nosql;
+
+CREATE TABLE `transactions` (
+    `id` varchar(35) GENERATED ALWAYS AS                 
+               (json_compact(json_extract(`trans_details`,'$._id'))) VIRTUAL,
+                `trans_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT 
+               NULL CHECK (json_valid(`trans_details`)),
+             UNIQUE KEY `id` (`id`),
+            CONSTRAINT `id_not_null` CHECK (`id` is not null)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
